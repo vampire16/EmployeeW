85d8ec14114b29557c56a547b1a14680711e5497
vampire16 <siddheshsalvi525@gmai.com>
2020-02-25 13:03:04 +0530

    ADDED : Employee attendance present or abscent

--- employeewage/EmployeeWage.sh
+++ employeewage/EmployeeWage.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 
 echo "Welcome"
+
+#CONSTANT VARIABLES
+ISPRESENT=1
+
+#RANDOM FUNCTION
+randomCheck=$((RANDOM%2))
+
+#CHECHING EMPLOYEE IS PRESENT OR ABSCENT
+if (( $randomCheck == $ISPRESENT ))
+then
+	echo "Employee is present"
+else
+	echo "Employee is abscent"
+fi
